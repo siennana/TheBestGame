ccfd003a58891a305c27c7f2a26623d6b919d1b5
siennana <sienna.kaylenb@gmail.com>
2020-02-09 02:14:17 -0800

    canvas update

---
+++ Assets/scripts/TreeCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeCollider : MonoBehaviour
+{
+    void onCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Tree collision");
+            print("collision");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
